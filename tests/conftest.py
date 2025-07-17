25c0a0313876abd74d9e855d8c35b74c33688ef4
ipl2 <ipl2@njit.edu>
2025-07-17 11:16:51 -0400

    corrected conftest to include os

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,6 +2,13 @@
 import pytest
 from httpx import AsyncClient
 from app.main import app  # Adjust import path as necessary
+from dotenv import load_dotenv # chgange
+import os # change here
+
+load_dotenv() # change
+
+assert os.getenv("ADMIN_USER") is not None, "ADMIN_USER env var not set"
+assert os.getenv("ADMIN_PASSWORD") is not None, "ADMIN_PASSWORD env var not set" # sanity checks
 
 @pytest.fixture
 async def client():
@@ -10,6 +17,6 @@ async def client():
 
 @pytest.fixture
 async def get_access_token_for_test(client):
-    form_data = {"username": "admin", "password": "secret"}
+    form_data = {"username": os.getenv("ADMIN_USER", "admin"), "password": os.getenv("ADMIN_PASSWORD", "secret")} # change
     response = await client.post("/token", data=form_data)
     return response.json()["access_token"]
